12c2b07bae71399237d44b67cd8d094024a44e89
Leandro Rodrigues <sj.leandro@gmail.com>
2023-11-06 00:40:19 -0300

    Ajusta código para entrega da Fase 3

--- src/main/kotlin/br/com/fiap/postech/lanchonete/adapter/outbound/repository/ClienteRepository.kt
+++ src/main/kotlin/br/com/fiap/postech/lanchonete/adapter/outbound/repository/ClienteRepository.kt
@@ -5,6 +5,6 @@ import org.springframework.data.domain.Page
 import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
-interface ClienteRepository:JpaRepository<Cliente, String> {
+interface ClienteRepository: JpaRepository<Cliente, String> {
     fun findByNome(nomeCliente: String, pageable: Pageable): Page<Cliente>
 }
